0f1d145deacacbaf21973ce3ae1f0ff8225cfc8a
khalilhasanzade <49582658+khalilhasanzade@users.noreply.github.com>
2021-12-27 01:31:19 +0400

    Update Dell_update.bat

--- Dell_update.bat
+++ Dell_update.bat
@@ -3,5 +3,3 @@
 "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe" /scan -silent -outputLog=C:\Dell-CU-scan.log
 
 "C:\Program Files (x86)\Dell\CommandUpdate\dcu-cli.exe" /applyUpdates -silent -reboot=enable -outputLog=C:\Dell-CU-apply.log
-
-"C:\Windows\notepad.exe"
