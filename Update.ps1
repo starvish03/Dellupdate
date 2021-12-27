2b0ac30732e0b4492c0fe9d943343ffc4e088282
ajmax03 <96718307+ajmax03@users.noreply.github.com>
2021-12-27 12:08:12 +0000

    Update Update.ps1

--- Update.ps1
+++ Update.ps1
@@ -1,11 +1,11 @@
 ﻿New-Item -Path 'C:\DellUpdate' -ItemType Directory -Force
-$sourceps1 = 'https://raw.githubusercontent.com/khalilhasanzade/Dellupdate/main/dell.ps1'
+$sourceps1 = 'https://raw.githubusercontent.com/ajmax03/Dellupdate/main/dell.ps1'
 $destinationps1 = 'C:\DellUpdate\Dell.ps1'
 Invoke-WebRequest -Uri $sourceps1 -OutFile $destinationps1
-$sourcemsg = 'https://raw.githubusercontent.com/khalilhasanzade/Dellupdate/main/usermsg.vbs'
+$sourcemsg = 'https://raw.githubusercontent.com/ajmax03/Dellupdate/main/usermsg.vbs'
 $destinationmsg = 'C:\DellUpdate\usermsg.vbs'
 Invoke-WebRequest -Uri $sourcemsg -OutFile $destinationmsg
-$sourcebatch = 'https://raw.githubusercontent.com/khalilhasanzade/Dellupdate/main/Dell_update.bat'
+$sourcebatch = 'https://raw.githubusercontent.com/ajmax03/Dellupdate/main/Dell_update.bat'
 $destinationbatch = 'C:\DellUpdate\Dell_update.bat'
 Invoke-WebRequest -Uri $sourcebatch -OutFile $destinationbatch
 PowerShell.exe -windowstyle hidden  -ExecutionPolicy Bypass -File  "C:\DellUpdate\Dell.ps1"
