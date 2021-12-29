40add922b0b6a4652a97222d247caeb1d85fcf7e
starvish03 <96815068+starvish03@users.noreply.github.com>
2021-12-29 08:00:48 +0000

    Update Update.ps1

--- Update.ps1
+++ Update.ps1
@@ -5,7 +5,4 @@ Invoke-WebRequest -Uri $sourceps1 -OutFile $destinationps1
 $sourcemsg = 'https://raw.githubusercontent.com/starvish03/Dellupdate/main/usermsg.vbs'
 $destinationmsg = 'C:\DellUpdate\usermsg.vbs'
 Invoke-WebRequest -Uri $sourcemsg -OutFile $destinationmsg
-$sourcebatch = 'https://raw.githubusercontent.com/ajmax03/Dellupdate/main/Dell_update.bat'
-$destinationbatch = 'C:\DellUpdate\Dell_update.bat'
-Invoke-WebRequest -Uri $sourcebatch -OutFile $destinationbatch
 PowerShell.exe -windowstyle hidden  -ExecutionPolicy Bypass -File  "C:\DellUpdate\Dell.ps1"
